9bd5df91f905dd7f0929f59a578b2273f3f431c0
Sebastian Tofano <sebastiantofano@gmail.com>
2026-04-17 19:07:10 -0300

    Use env var for base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+const base = process.env.GITHUB_PAGES ? '/funeraria' : '/';
+
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
   build: {
     format: 'directory'
   },
-  base: '/funeraria'
+  base
 });
