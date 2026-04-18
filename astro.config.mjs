a66ee0674923ceb3b2fe1996ab1be2f5c4ded9cf
Sebastian Tofano <sebastiantofano@gmail.com>
2026-04-17 23:00:44 -0300

    Fix nav links with base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-const base = process.env.GITHUB_PAGES ? '/funeraria' : '/';
-
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
   build: {
     format: 'directory'
   },
-  base
+  base: '/funeraria'
 });
